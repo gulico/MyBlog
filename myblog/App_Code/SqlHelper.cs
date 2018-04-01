dfd5b414b291d736900dd8c056154e31bbd78eda
wxy <767293861@qq.com>
2018-04-01 22:52:16 +0800

    创建工程。

---
+++ myblog/App_Code/SqlHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;//引用SQL Server数据库
+using System.Data;//引用DataSet
+using System.Web.UI;//引用Page对象
+using System.Text;//引用StringBuilder对象
+
+/// <summary>
+/// SqlHelper 的摘要说明
+/// </summary>
+public class SqlHelper
+{
+	public SqlHelper()
+	{
+		//
+		// TODO: 在此处添加构造函数逻辑
+		//
+	}
+
+}
